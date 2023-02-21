43e37911292206a92b0d680ceedd0aa675e0fef4
hamzasohail445 <113169600+hamzasohail445@users.noreply.github.com>
2023-02-21 12:28:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function begin(){
+    const length = 15; 
+    let value = 5;
+    value = 10; 
+    for (var index = 0; index < 5; index++){
+        console.log(index,length)
+        console.log(value)
+    }
+    console.log(index)
+}
+begin()
